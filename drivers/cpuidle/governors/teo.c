d1cadfd685d4c99a69061f64ed914d7669333068
Sultan Alsawaf <sultan@kerneltoast.com>
2023-12-11 23:26:25 -0800

    cpuidle: teo: Treat negative sleep lengths as indefinite
    A negative sleep length means there's no hrtimer armed, and therefore the
    sleep length is indefinite if the tick is stopped. Fix this logic in TEO to
    allow a deeper idle state to correctly be used when there's no hrtimer
    armed and thus the CPU won't be prematurely woken up from its sleep.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    (cherry picked from commit bc8ad9537b3846c596db241fc17cc3efa77e83ad)

--- drivers/cpuidle/governors/teo.c
+++ drivers/cpuidle/governors/teo.c
@@ -562,6 +562,46 @@ static int teo_select(struct cpuidle_driver *drv, struct cpuidle_device *dev,
 			idx = teo_find_shallower_state(drv, dev, idx, delta_tick, false);
 	}
 
+	/*
+	 * Skip the timers check if state 0 is the current candidate one,
+	 * because an immediate non-timer wakeup is expected in that case.
+	 */
+	if (!idx)
+		goto out_tick;
+
+	/*
+	 * If state 0 is a polling one, check if the target residency of
+	 * the current candidate state is low enough and skip the timers
+	 * check in that case too.
+	 */
+	if ((drv->states[0].flags & CPUIDLE_FLAG_POLLING) &&
+	    drv->states[idx].target_residency_ns < RESIDENCY_THRESHOLD_NS)
+		goto out_tick;
+
+	duration_ns = tick_nohz_get_sleep_length(&delta_tick);
+	if (duration_ns <= 0)
+		duration_ns = S64_MAX;
+	cpu_data->sleep_length_ns = duration_ns;
+
+	/*
+	 * If the closest expected timer is before the terget residency of the
+	 * candidate state, a shallower one needs to be found.
+	 */
+	if (drv->states[idx].target_residency_ns > duration_ns) {
+		i = teo_find_shallower_state(drv, dev, idx, duration_ns, false);
+		if (teo_state_ok(i, drv))
+			idx = i;
+	}
+
+	/*
+	 * If the selected state's target residency is below the tick length
+	 * and intercepts occurring before the tick length are the majority of
+	 * total wakeup events, do not stop the tick.
+	 */
+	if (drv->states[idx].target_residency_ns < TICK_NSEC &&
+	    tick_intercept_sum > cpu_data->total / 2 + cpu_data->total / 8)
+		duration_ns = TICK_NSEC / 2;
+
 end:
 	/*
 	 * Allow the tick to be stopped unless the selected state is a polling
